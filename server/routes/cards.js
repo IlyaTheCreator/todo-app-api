0850159284b1193f7210125109566bebc5ebbaa1
ilyaTheCreator <respondatanytime@gmail.com>
2022-04-14 11:02:29 +0300

    Describe Card schema

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -3,10 +3,40 @@ const cardController = require('../controllers/cardController');
 
 const router = new Router();
 
-router.post('/card', cardController.addCard);
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Card:
+ *       type: object
+ *       required:
+ *         - name
+ *         - isCompleted
+ *         - listId
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the card
+ *         name:
+ *           type: string
+ *           description: Text of a task
+ *         isCompleted:
+ *           type: boolean
+ *           description: The card completion state
+ *         listId:
+ *           type: string
+ *           description: Id of a list the card belongs to
+ *       example:
+ *         id: d5fE_asz
+ *         name: Throw out the trash
+ *         isCompleted: false
+ *         listId: 3
+ */
+
 router.get('/cards', cardController.getCards);
 router.get('/cards/filter', cardController.filterCards);
 router.get('/card/:id', cardController.getCard);
+router.post('/card', cardController.addCard);
 router.put('/card/:id', cardController.setNameCard);
 router.put('/card/complete/:id', cardController.setCompleted);
 router.delete('/card/:id', cardController.deleteCard);

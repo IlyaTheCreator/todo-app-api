d53aca1de74c1f3909f37e80fe9c618edca9f760
ilyaTheCreator <respondatanytime@gmail.com>
2022-04-14 10:47:28 +0300

    Swagger docs config

--- server/index.js
+++ server/index.js
@@ -1,20 +1,47 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 
-const sequelize = require('./db/database');
+const sequelize = require("./db/database");
 const cardRoutes = require("./routes/cards");
 const listRoutes = require("./routes/lists");
+const swaggerJsdoc = require("swagger-jsdoc");
 
 const PORT = process.env.PORT || 8080;
 
-sequelize.sync().then(() => console.log('db is ready'));
+sequelize.sync().then(() => console.log("db is ready"));
+
+// Swagger options
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Yandex Test Todo API",
+      version: "1.0.0",
+      description: "A simple API for managing todo lists",
+    },
+    servers: [
+      {
+        url: `http://localhost:${PORT}`,
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+// Initializing swagger jsdoc
+const specs = swaggerJsdoc(options);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api', cardRoutes);
-app.use('/api', listRoutes);
+app.use("/api", cardRoutes);
+app.use("/api", listRoutes);
+
+// Defining a separate route for docs page
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 const start = async () => {
   try {

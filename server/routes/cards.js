ba746539c1553fae148ef2af81a9b8d3c388746f
ilya klimov <respondatanytime@gmail.com>
2022-04-15 10:47:52 +0300

    Add error schema

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -35,6 +35,35 @@ const router = new Router();
 
 /**
  * @swagger
+ * components:
+ *   schemas:
+ *     Error:
+ *       type: object
+ *       properties:
+ *         statusCode:
+ *           type: number
+ *           description: |
+ *            Custom error type. Possible values are:
+ *            * 1 - property is not defined
+ *            * 2 - name must be unique
+ *            * 3 - isCompleted is not boolean
+ *            * 4 - name is not a string
+ *            * 5 - listId is not a number
+ *            * 6 - property value is incorrect
+ *            * 7 - card is not defined
+ *            * 8 - property cannot be empty
+ *            * 9 - list is not defined
+ *            * 10 - no entity with provided id
+ *         message:
+ *           type: string
+ *           description: Text of an error
+ *       example:
+ *         statusCode: 4
+ *         message: The value of IsCompleted can be either true(1) or false(0)
+ */
+
+/**
+ * @swagger
  * tags:
  *   name: Cards
  *   description: Managing cards

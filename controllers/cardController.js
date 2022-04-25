b59e30adbb4e407d4c6a69a86d3a797a6d2d3aaa
Artem Lobovskii <a.lobovskiy@gmail.com>
2022-04-25 09:46:27 +0300

    rename controller

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -1,47 +1,47 @@
 const { Op } = require("sequelize");
 
-const Cards = require("../db/tables/Cards");
+const Items = require("../db/tables/Items");
 const BaseController = require("./baseController");
 const { errors } = require("../rules/errors");
 const { messages } = require("../rules/messages");
 
-class CardController extends BaseController {
+class ItemsController extends BaseController {
   constructor() {
-    super(Cards);
+    super(Items);
   }
   
   /**
    * POST запрос. Добавление карточки. Данные принимаются из тела запрсоа
-   * ex. http://localhost:8080/api/cards
+   * ex. http://localhost:8080/api/items
    */
-  addCard = async (req, res) => {
+  addItem = async (req, res) => {
     try {
       const { name, parentId } = req.body;
 
       if (parentId) {
-        const parentCard = await Cards.findOne({ where: { id: parentId } });
+        const parentItem = await Items.findOne({ where: { id: parentId } });
 
-        if (!parentCard) {
-          res.status(400).json(errors.cards.noParent);
+        if (!parentItem) {
+          res.status(400).json(errors.items.noParent);
 
           return;
         }
       }
 
-      const card = parentId ? { name, parentId } : { name };
-      const cardError = this.validate(card, CardController.types);
+      const item = parentId ? { name, parentId } : { name };
+      const itemError = this.validate(item, ItemsController.types);
 
-      if (cardError) {
-        res.status(400).json(cardError);
+      if (itemError) {
+        res.status(400).json(itemError);
 
         return;
       }
 
-      const data = await Cards.create(card);
+      const data = await Items.create(item);
 
       res.status(201).json({
         id: data.id,
-        ...messages.card.added
+        ...messages.items.added
       });
     } catch (e) {
       res.status(500).json(e);
@@ -50,17 +50,17 @@ class CardController extends BaseController {
 
   /**
    * GET запрос. Получение всех карточек
-   * ex. http://localhost:8080/api/cards
+   * ex. http://localhost:8080/api/items
    */
-  async getCards(req, res) {
+  async getItems(req, res) {
     try {
-      const cards = await Cards.findAll();
+      const items = await Items.findAll();
 
-      if (!cards) {
-        res.json(messages.card.noData);
+      if (!items) {
+        res.json(messages.items.noData);
       }
 
-      res.json(cards);
+      res.json(items);
     } catch (e) {
       res.status(500).json(e);
     }
@@ -68,23 +68,23 @@ class CardController extends BaseController {
 
   /**
    * GET запрос. Получение карточки по id
-   * ex. http://localhost:8080/api/cards/:id
+   * ex. http://localhost:8080/api/items/:id
    */
-  async getCard(req, res) {
+  async getItem(req, res) {
     try {
       const reqId = req.params.id;
-      const card = await Cards.findOne({ where: { id: reqId } });
+      const item = await Items.findOne({ where: { id: reqId } });
 
-      if (!card) {
-        res.status(400).json(errors.cards.notDefined);
+      if (!item) {
+        res.status(400).json(errors.items.notDefined);
 
         return;
       }
 
-      const cardChildren = await Cards.findAll({ where: { parentId: card.id } });
-      card.dataValues.children = cardChildren;
+      const itemChildren = await Items.findAll({ where: { parentId: item.id } });
+      item.dataValues.children = itemChildren;
 
-      res.json(card);
+      res.json(item);
     } catch (e) {
       res.status(500).json(e);
     }
@@ -92,35 +92,35 @@ class CardController extends BaseController {
 
   /**
    * PUT запрос. Обновление имени для карточки
-   * ex. http://localhost:8080/api/cards/:id
+   * ex. http://localhost:8080/api/items/:id
    * Имя принимает из тела запроса
    */
-  setNameCard = async (req, res) => {
+  setNameItem = async (req, res) => {
     try {
       const reqId = req.params.id;
-      const card = await Cards.findOne({ where: { id: reqId } });
+      const item = await Items.findOne({ where: { id: reqId } });
 
-      if (!card) {
-        res.status(400).json(errors.cards.notDefined);
+      if (!item) {
+        res.status(400).json(errors.items.notDefined);
 
         return;
       }
 
       const { name } = req.body;
-      const cardError = this.validate({ name }, CardController.types);
+      const itemError = this.validate({ name }, ItemsController.types);
 
-      if (cardError) {
-        res.status(400).json(cardError);
+      if (itemError) {
+        res.status(400).json(itemError);
 
         return;
       }
 
-      card.name = name;
-      await card.save();
+      item.name = name;
+      await item.save();
 
       res.status(200).json({
-        id: card.id,
-        ...messages.card.updated
+        id: item.id,
+        ...messages.items.updated
       });
     } catch (e) {
       res.status(400).json(e);
@@ -129,24 +129,24 @@ class CardController extends BaseController {
 
   /**
    * PUT запрос. Меняет флаг isCompleted
-   * ex. http://localhost:8080/api/card/complete/:id
+   * ex. http://localhost:8080/api/items/complete/:id
    */
   async setCompleted(req, res) {
     try {
       const reqId = req.params.id;
-      const card = await Cards.findOne({ where: { id: reqId } });
+      const item = await Items.findOne({ where: { id: reqId } });
 
-      if (!card) {
-        res.status(400).json(errors.cards.notDefined);
+      if (!item) {
+        res.status(400).json(errors.items.notDefined);
 
         return;
       }
-      card.isCompleted = !card.isCompleted;
-      await card.save();
+      item.isCompleted = !item.isCompleted;
+      await item.save();
 
       res.status(200).json({
-        id: card.id,
-        ...messages.card.updated
+        id: item.id,
+        ...messages.items.updated
       });
     } catch (e) {
       res.status(500).json(e);
@@ -155,24 +155,24 @@ class CardController extends BaseController {
 
   /**
    * DELETE запрос. Удаление карточки
-   * ex. http://localhost:8080/api/cards/:id
+   * ex. http://localhost:8080/api/items/:id
    */
-  async deleteCard(req, res) {
+  async deleteItem(req, res) {
     try {
       const reqId = req.params.id;
-      const card = await Cards.findOne({ where: { id: reqId } });
+      const item = await Items.findOne({ where: { id: reqId } });
 
-      if (card) {
-        await Cards.destroy({ where: { id: reqId } });
+      if (item) {
+        await Items.destroy({ where: { id: reqId } });
         res.status(200).json({
-          id: card.id,
-          ...messages.card.deleted
+          id: item.id,
+          ...messages.items.deleted
         });
 
         return;
       }
 
-      res.status(400).json(errors.cards.notDefined);
+      res.status(400).json(errors.items.notDefined);
     } catch (e) {
       res.status(500).json(e);
     }
@@ -180,17 +180,17 @@ class CardController extends BaseController {
 
   /**
    * Фильтр запросов через параметры
-   * ex. http://localhost:8080/api/cards/filter?key=value
-   * ex. http://localhost:8080/api/cards/filter?isCompleted=true&parentId=1
+   * ex. http://localhost:8080/api/items/filter?key=value
+   * ex. http://localhost:8080/api/items/filter?isCompleted=true&parentId=1
   */
-  filterCards = async (req, res) => {
+  filterItems = async (req, res) => {
     try {
       const where = {};
 
       for (const key in req.query) {
         if (Object.hasOwnProperty.call(req.query, key)) {
           const value = req.query[key];
-          if (Object.keys(CardController.types).includes(key.toLowerCase())) {
+          if (Object.keys(ItemsController.types).includes(key.toLowerCase())) {
             let booleanValue;
 
             if (value === 'true') {
@@ -214,9 +214,9 @@ class CardController extends BaseController {
           }
         }
       }
-      const cards = await Cards.findAll({ where });
+      const items = await Items.findAll({ where });
 
-      res.status(200).json(cards);
+      res.status(200).json(items);
 
       return;
     } catch (e) {
@@ -227,13 +227,13 @@ class CardController extends BaseController {
   /**
    * DELETE запрос
    * Удаление всех карточек
-   * ex. http://localhost:8080/api/cards
+   * ex. http://localhost:8080/api/items
    */
   async deleteAll(req, res) {
     try {
-      Cards.destroy({ truncate: true });
+      Items.destroy({ truncate: true });
 
-      res.json(messages.card.deletedAll);
+      res.json(messages.items.deletedAll);
     } catch (e) {
       res.status(500).json(e);
     }
@@ -242,21 +242,21 @@ class CardController extends BaseController {
   /**
    * PUT запрос
    * Пометить сразу все карточки выполненными/невыполненными
-   * ex. http://localhost:8080/api/cards/complete/all/false
-   * ex. http://localhost:8080/api/cards/complete/all/true
+   * ex. http://localhost:8080/api/items/complete/all/false
+   * ex. http://localhost:8080/api/items/complete/all/true
    */
   async toggleCompleteAll(req, res) {
     try {
       const reqBoolean = req.params.boolean;
       if (reqBoolean === 'true' || reqBoolean === 'false') {
         const boolean = reqBoolean === 'true' ? true : false;
-        await Cards.update({ isCompleted: boolean }, { where: { isCompleted: !boolean } });
-        res.json(messages.card.updatedAll);
+        await Items.update({ isCompleted: boolean }, { where: { isCompleted: !boolean } });
+        res.json(messages.items.updatedAll);
 
         return;
       }
 
-      res.status(400).json(errors.cards.incorrectlyProp);
+      res.status(400).json(errors.items.incorrectlyProp);
     } catch (e) {
       res.status(500).json(e);
     }
@@ -265,17 +265,17 @@ class CardController extends BaseController {
   /**
    * DELETE запрос
    * Удаление всех выполненных карточек
-   * ex. http://localhost:8080/api/cards/complete/all
+   * ex. http://localhost:8080/api/items/complete/all
    */
   async deleteComplete(req, res) {
     try {
-      await Cards.destroy({ where: { isCompleted: true } });
+      await Items.destroy({ where: { isCompleted: true } });
 
-      res.json(messages.card.deleteComplete);
+      res.json(messages.items.deleteComplete);
     } catch (e) {
       res.status(500).json(e);
     }
   }
 }
 
-module.exports = new CardController();
+module.exports = new ItemsController();
